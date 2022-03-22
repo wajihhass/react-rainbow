3bd0ad2c18bad0f518c2ef83f79bff93ae1fe11a
Wajih <hass_wajih@hotmail.com>
2022-03-22 12:39:23 -0400

    reactRainbow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import ColorBlock from './components/ColorBlock';
+import ColorForm from './components/ColorForm';
 
 function App() {
+  let [colors, setColors] = useState(["green", "violet", "red", "yellow"]);
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
+
+  let colorMap = colors.map((color, i) => {
+    return <ColorBlock color={color} key={i} />
+  })
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {colorMap}
+        <ColorForm addColor={addColor} />
     </div>
   );
 }
